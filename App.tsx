0298ad869b9f761e64740f79b8e4991616dbd494
TonySilva7 <tony.deve7oper@gmail.com>
2024-02-24 16:51:22 -0300

    chore: config eslint in project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignIn from './src/screens/SignIn';
+import SignIn from './src/screens/SignIn'
 
 export default function App() {
-  return (
-    <SignIn />
-  );
+  return <SignIn />
 }
-
