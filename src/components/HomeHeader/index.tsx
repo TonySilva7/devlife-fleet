7910179b0b309f49e27b2d8ab2035a46ae14cd40
TonySilva7 <tony.deve7oper@gmail.com>
2024-03-04 16:04:09 -0300

    chore: add phosphor icons

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Container, Greeting, Message, Name } from './styles'
+import { TouchableOpacity } from 'react-native'
+import { Power } from 'phosphor-react-native'
+import { useTheme } from 'styled-components/native'
 
 export function HomeHeader() {
+  const { COLORS } = useTheme()
   return (
     <Container>
       <Greeting>
@@ -9,6 +13,10 @@ export function HomeHeader() {
 
         <Name>Rodrigo</Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power size={32} color={COLORS.GRAY_400} />
+      </TouchableOpacity>
     </Container>
   )
 }
