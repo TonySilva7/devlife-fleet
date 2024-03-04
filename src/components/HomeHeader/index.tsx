f7e802b265ada415b63e2e174b4215d654d8e3f8
TonySilva7 <tony.deve7oper@gmail.com>
2024-03-04 17:13:53 -0300

    feat: implement login/logout on realm and mongo atlas

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Container, Greeting, Message, Name } from './styles'
+import { Container, Greeting, Message, Name, Picture } from './styles'
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 import { useTheme } from 'styled-components/native'
+import { useUser, useApp } from '@realm/react'
 
 export function HomeHeader() {
   const { COLORS } = useTheme()
+  const user = useUser()
+  const app = useApp()
+
+  function handleLogOut() {
+    app.currentUser?.logOut()
+  }
+
   return (
     <Container>
+      <Picture
+        source={{ uri: user?.profile.pictureUrl }}
+        placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
+      />
+
       <Greeting>
         <Message>Olá</Message>
 
-        <Name>Rodrigo</Name>
+        <Name>{user.profile.name}</Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogOut}>
         <Power size={32} color={COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
